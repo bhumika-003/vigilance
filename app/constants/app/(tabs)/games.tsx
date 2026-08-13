8a63a5f377bfdbcd63f3d02951d5279757f65be3
disharimandal74-sys <disharimandal74@gmail.com>
2026-08-14 01:50:26 +0530

    Sync all completed frontend work

---
+++ app/constants/app/(tabs)/games.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function GamesScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 24 }}>Games Tab</Text>
+    </View>
+  );
+}
